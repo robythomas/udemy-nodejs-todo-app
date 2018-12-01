7aee16538f26481a0f73815d025b6eff5c777550
Roby Thomas <roby.thomas@gmail.com>
2018-12-01 14:27:38 +0530

    Add Rest API to update Todo

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -10,6 +11,17 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var newTodo = new Todo({
+    text: req.body.text
+  });
+  newTodo.save().then((todo) => {
+    res.status(201).send({todo});
+  }, (err) => {
+    res.status(400).send(`Failed to save the todo item - ${err}`);
+  });
+});
 
 app.get('/todos', (req, res) => {
   Todo.find({}).then((todos) => {
@@ -38,15 +50,41 @@ app.get('/todos/:id', (req, res) => {
   }
 });
 
-app.post('/todos', (req, res) => {
-  var newTodo = new Todo({
-    text: req.body.text
-  });
-  newTodo.save().then((doc) => {
-    res.status(201).send(doc);
-  }, (err) => {
-    res.status(400).send(`Failed to save the todo item - ${err}`);
-  });
+app.patch('/todos/:id', (req, res) => {
+  if(!ObjectID.isValid(req.params.id)) {
+    res.status(400).send("Your request has provided invalid data");
+  }
+  else {
+    var body = req.body;
+    Todo.findById(req.params.id).then((todo) => {
+      if(!todo) {
+        return res.status(404).send(`Failed to find a todo by id ${req.params.id} to update`);
+      }
+
+      if(_.isBoolean(body.completed)) {
+        if(body.completed && !todo.completed) {
+          body.completedAt = new Date().getTime();
+        }
+        else if(!body.completed && todo.completed) {
+          body.completedAt = null;
+        }
+      }
+
+      Todo.findByIdAndUpdate(req.params.id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+          res.status(404).send(`Failed to find a todo by id ${req.params.id} to update`);
+        }
+        else {
+          res.status(200).send({todo});
+        }
+      }, (err) => {
+        console.log(err);
+        res.status(500).send("Failed to update the todo item");
+      });
+    }, (err) => {
+
+    });
+  }
 });
 
 app.delete('/todos/:id', (req, res) => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -17,7 +17,9 @@ var todoSeed = [
   },
   {
     _id: ObjectID(),
-    text: "Third todo"
+    text: "Third todo",
+    completed: true,
+    completedAt: 12345
   }
 ];
 
@@ -36,7 +38,7 @@ describe('POST /todos', () => {
     request(app).post('/todos').send({text})
       .expect(201)
       .expect((res) => {
-        expect(res.body.text).to.equal(text);
+        expect(res.body.todo.text).to.equal(text);
       })
       .end((err, res) => {
         if(err) {
@@ -105,6 +107,181 @@ describe('GET /todos/:id', () => {
   });
 });
 
+describe("PATCH /tools/:id", () => {
+  it("should update a todo when valid id and content is provided", (done) => {
+    var text = 'Update first todo to completed';
+    request(app).patch(`/todos/${todoSeed[0]._id}`).send({
+      text: text,
+      completed: true
+    })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).to.equal(text);
+        expect(res.body.todo.completed).to.be.true;
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        Todo.findById(todoSeed[0]._id)
+          .then((todo) => {
+            expect(todo.text).to.equal(text);
+            expect(todo.completed).to.be.true;
+            done();
+          })
+          .catch((e) => done(e));
+      });
+  });
+
+  it("should set completedAt if request marks completion for uncompleted todo", (done) => {
+    var startTime = new Date().getTime();
+    request(app).patch(`/todos/${todoSeed[0]._id}`).send({
+      completed: true
+    })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).to.equal(todoSeed[0].text);
+        expect(res.body.todo.completed).to.be.true;
+        expect(res.body.todo.completedAt).not.to.be.null;
+        expect(res.body.todo.completedAt).to.be.a('number');
+        expect(res.body.todo.completedAt).to.be.above(startTime);
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        Todo.findById(todoSeed[0]._id)
+          .then((todo) => {
+            expect(todo.completed).to.be.true;
+            expect(todo.completedAt).not.to.be.null;
+            expect(todo.completedAt).to.be.a('number');
+            expect(todo.completedAt).to.be.above(startTime);
+            done();
+          })
+          .catch((e) => done(e));
+      });
+  });
+
+  it("should not update completedAt if request marks completion for already completed todo", (done) => {
+    request(app).patch(`/todos/${todoSeed[2]._id}`).send({
+      completed: true
+    })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).to.equal(todoSeed[2].text);
+        expect(res.body.todo.completed).to.be.true;
+        expect(res.body.todo.completedAt).not.to.be.null;
+        expect(res.body.todo.completedAt).to.be.equal(todoSeed[2].completedAt);
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        Todo.findById(todoSeed[2]._id)
+          .then((todo) => {
+            expect(todo.completed).to.be.true;
+            expect(todo.completedAt).not.to.be.null;
+            expect(todo.completedAt).to.be.equal(todoSeed[2].completedAt);
+            done();
+          })
+          .catch((e) => done(e));
+      });
+  });
+
+  it("should unset completedAt if request removes completion for already completed todo", (done) => {
+    request(app).patch(`/todos/${todoSeed[2]._id}`).send({
+      completed: false
+    })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).to.equal(todoSeed[2].text);
+        expect(res.body.todo.completed).to.be.false;
+        expect(res.body.todo.completedAt).to.be.null;
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        Todo.findById(todoSeed[2]._id)
+          .then((todo) => {
+            expect(todo.completed).to.be.false;
+            expect(todo.completedAt).to.be.null;
+            done();
+          })
+          .catch((e) => done(e));
+      });
+  });
+
+  it("should return a Not Found message when valid non existing id is provided", (done) => {
+    var validNonExistingId = ObjectID();
+    var text = 'Update first todo to completed';
+
+    request(app).patch(`/todos/${validNonExistingId._id}`).send({
+      text: text,
+      completed: true
+    })
+      .expect(404)
+      .end(done);
+  });
+
+  it("should return a Bad Request message when invalid id is provided", (done) => {
+    var invalidId = "5c020351c412a12e07d26864Abc1d";
+    var text = 'Update first todo to completed';
+
+    request(app).patch(`/todos/${invalidId}`).send({
+      text: text,
+      completed: true
+    })
+      .expect(400)
+      .end(done);
+  });
+
+  it("should return a Bad Request message when invalid content is provided", (done) => {
+    var invalidId = "5c020351c412a12e07d26864Abc1d";
+    var text = 'Update first todo to completed';
+
+    request(app).patch(`/todos/${invalidId}`).send({
+      text: text,
+      completed: "Another text"
+    })
+      .expect(400)
+      .end(done);
+  });
+
+  it("should not update forbidden fields provided in request", (done) => {
+    var startTime = new Date().getTime();
+    var text = 'Update first todo to completed';
+    var bogusTime = new Date().getTime();
+
+    request(app).patch(`/todos/${todoSeed[0]._id}`).send({
+      text: text,
+      completed: true,
+      completedAt: 12345
+    })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).to.equal(text);
+        expect(res.body.todo.completed).to.be.true;
+        expect(res.body.todo.completedAt).to.be.not.equal(12345);
+        expect(res.body.todo.completedAt).to.be.above(startTime);
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        Todo.findById(todoSeed[0]._id)
+          .then((todo) => {
+            expect(todo.text).to.equal(text);
+            expect(todo.completed).to.be.true;
+            expect(res.body.todo.completedAt).to.be.not.equal(12345);
+            expect(res.body.todo.completedAt).to.be.above(startTime);
+            done();
+          })
+          .catch((e) => done(e));
+        });
+  });
+});
+
 describe("DELETE /todos/:id", () => {
   it("should delete a todo when valid id is provided", (done) => {
     request(app).delete(`/todos/${todoSeed[0]._id}`)
@@ -119,12 +296,14 @@ describe("DELETE /todos/:id", () => {
         }).catch((e) => done(e));
       });
   });
+
   it("should return a Not Found response when a valid non-existing id is provided", (done) => {
     var validNonExistingId = ObjectID();
     request(app).delete(`/todos/${validNonExistingId._id}`)
       .expect(404)
       .end(done);
   });
+
   it("should return a Bad Request response when an invalid id is provided", (done) => {
     var invalidId = "5c020351c412a12e07d26864Abc1d";
     request(app).delete(`/todos/${invalidId}`)
